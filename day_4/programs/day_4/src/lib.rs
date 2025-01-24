3edba08c87b60f8c128448815a84d21ef3c23d85
Sumit Guha <sumit03guha@gmail.com>
2025-01-24 18:28:50 +0530

    add range validation and error handling in day_4 program with corresponding tests

--- day_4/programs/day_4/src/lib.rs
+++ day_4/programs/day_4/src/lib.rs
@@ -6,11 +6,32 @@ declare_id!("EabHrNd6S9eftdasCnAiz4zKosGWZcS98upxr5o5xqCf");
 pub mod day_4 {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn limit_range(ctx: Context<Initialize>, number: u32) -> Result<()> {
         msg!("Greetings from: {:?}", ctx.program_id);
+        require!(number < 10, NumberError::NumberGreaterThan10);
+        require!(number > 3, NumberError::NumberLesserThan3);
         Ok(())
+    }
+
+    pub fn this_function_always_reverts(_ctx: Context<Initialize>) -> Result<()> {
+        msg!("will this print?");
+        return err!(MyError::AlwaysReverts)
     }
 }
 
 #[derive(Accounts)]
 pub struct Initialize {}
+
+#[error_code]
+pub enum NumberError {
+    #[msg("number is not less than 10")]
+    NumberGreaterThan10,
+    #[msg("number is not greater than 3")]
+    NumberLesserThan3
+}
+
+#[error_code]
+pub enum MyError {
+    #[msg("This always reverts")]
+    AlwaysReverts
+}
--- day_4/tests/day_4.ts
+++ day_4/tests/day_4.ts
@@ -1,16 +1,53 @@
-import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
-import { Day4 } from "../target/types/day_4";
+import * as anchor from '@coral-xyz/anchor';
+import { Program, AnchorError } from '@coral-xyz/anchor';
+import { Day4 } from '../target/types/day_4';
+import { assert } from 'chai';
 
-describe("day_4", () => {
+describe('day_4', () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
   const program = anchor.workspace.Day4 as Program<Day4>;
 
-  it("Is initialized!", async () => {
+  it('tests number greater than 10', async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    try {
+      const tx = await program.methods.limitRange(71).rpc();
+      console.log('Your transaction signature', tx);
+    } catch (_err) {
+      console.error('raw Error', _err);
+      const err: AnchorError = _err;
+      const errMsg = 'number is not less than 10';
+
+      assert.strictEqual(err.error.errorMessage, errMsg);
+      console.log('Error code : ', err.error.errorCode);
+    }
+  });
+
+  it('tests number less than 3', async () => {
+    try {
+      const tx = await program.methods.limitRange(1).rpc();
+      console.log('Your transaction signature', tx);
+    } catch (_err) {
+      console.error('raw Error', _err);
+      const err: AnchorError = _err;
+      const errMsg = 'number is not greater than 3';
+
+      assert.strictEqual(err.error.errorMessage, errMsg);
+      console.log('Error code : ', err.error.errorCode);
+    }
+  });
+
+  it('tests always reverts', async () => {
+    try {
+      const tx = await program.methods.thisFunctionAlwaysReverts().rpc();
+      console.log('Your transaction signature', tx);
+    } catch (_err) {
+      console.error('raw Error', _err);
+      const err: AnchorError = _err;
+
+      assert.strictEqual(err.error.errorMessage, 'This always reverts');
+      console.log('Error code : ', err.error.errorCode);
+    }
   });
 });
