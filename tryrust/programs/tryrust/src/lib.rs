8d0ef353917de4ec54cc6c43c4f985b80519af38
Sumit Guha <sumit03guha@gmail.com>
2025-01-28 16:08:00 +0530

    add age checking functions and hashmap example in tryrust program with corresponding tests

--- tryrust/programs/tryrust/src/lib.rs
+++ tryrust/programs/tryrust/src/lib.rs
@@ -10,6 +10,37 @@ pub mod tryrust {
         msg!("Greetings from: {:?}", ctx.program_id);
         Ok(())
     }
+
+    pub fn age_checker(_ctx: Context<Initialize>, age: u8) -> Result<()> {
+        if age < 18 {
+            msg!("You are below 18.")
+        } else {
+            msg!("You are 18 or above.")
+        }
+        Ok(())
+    }
+
+    pub fn check_age_with_match(_ctx: Context<Initialize>, age: u8) -> Result<()> {
+        match age {
+            0..=17 => msg!("match : You are below 18."),
+            _ => msg!("match : You are 18 or above.")
+        }
+        Ok(())
+    }
+
+    pub fn trying_hasmap(_ctx: Context<Initialize>, name: String, age: u8) -> Result<()> {
+        use std::collections::HashMap;
+
+        let mut id_hashmap = HashMap::new();
+
+        id_hashmap.insert(&name, age);
+
+        let id_age = id_hashmap.get(&name).unwrap();
+
+        msg!(&format!("The identity is :: name : {}, age : {}", &name, id_age));
+
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
--- tryrust/tests/tryrust.ts
+++ tryrust/tests/tryrust.ts
@@ -1,16 +1,31 @@
-import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
-import { Tryrust } from "../target/types/tryrust";
+import * as anchor from '@coral-xyz/anchor';
+import { Program } from '@coral-xyz/anchor';
+import { Tryrust } from '../target/types/tryrust';
 
-describe("tryrust", () => {
+describe('tryrust', () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
   const program = anchor.workspace.Tryrust as Program<Tryrust>;
 
-  it("Is initialized!", async () => {
+  it('Is initialized!', async () => {
     // Add your test here.
     const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    console.log('Your transaction signature', tx);
+  });
+
+  it('checks age', async () => {
+    const tx = await program.methods.ageChecker(18).rpc();
+    console.log('Your transaction signature', tx);
+  });
+
+  it('checks age with match', async () => {
+    const tx = await program.methods.checkAgeWithMatch(17).rpc();
+    console.log('Your transaction signature', tx);
+  });
+
+  it('tests hashmap', async () => {
+    const tx = await program.methods.tryingHasmap('Alex', 24).rpc();
+    console.log('Your transaction signature', tx);
   });
 });
