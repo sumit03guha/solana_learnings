cf84f7eda2a0d76a5aba02c7700290799be31933
Sumit Guha <sumit03guha@gmail.com>
2025-02-03 17:14:34 +0530

    add support for multiple signers and owner-only function in accounts-and-signers program

--- accounts-and-signers/programs/accounts-and-signers/src/lib.rs
+++ accounts-and-signers/programs/accounts-and-signers/src/lib.rs
@@ -2,6 +2,8 @@ use anchor_lang::prelude::*;
 
 declare_id!("GgupEyyqv3EXoBjKKXFAPiTzjH3aHBsXcjhAyisUd2q1");
 
+const OWNER: &str = "2cZvfJjkdkzoeVCEZsdtjQWmA46DcTcKD4HgyizcmVZs";
+
 #[program]
 pub mod accounts_and_signers {
     use super::*;
@@ -16,9 +18,37 @@ pub mod accounts_and_signers {
         msg!("Namaste from {:?}", signer);
         Ok(())
     }
+
+    pub fn call_from_multiple_signers(ctx: Context<Initialize>) -> Result<()> {
+        let signer = &ctx.accounts.signer;
+        let signer2 = &ctx.accounts.signer2;
+        let signer3 = &ctx.accounts.signer3;
+        msg!("Namaste from : {:?}, {:?}, {:?}", signer, signer2, signer3);
+        Ok(())
+    }
+
+    #[access_control(only_owner(&ctx))]
+    pub fn only_owner_function(ctx: Context<Initialize>) -> Result<()> {
+        msg!("I am the owner");
+        Ok(())
+    }
+
+}
+
+fn only_owner(ctx: &Context<Initialize>) -> Result<()> {
+    require_keys_eq!(ctx.accounts.signer.key(), OWNER.parse::<Pubkey>().unwrap(), OwnerError::NotOwner);
+    Ok(())
 }
 
 #[derive(Accounts)]
 pub struct Initialize<'info> {
-    pub signer: Signer<'info>
+    pub signer: Signer<'info>,
+    pub signer2: Signer<'info>,
+    pub signer3: Signer<'info>
+}
+
+#[error_code]
+pub enum OwnerError {
+    #[msg("Only the owner can call this function")]
+    NotOwner
 }
--- accounts-and-signers/tests/accounts-and-signers.ts
+++ accounts-and-signers/tests/accounts-and-signers.ts
@@ -1,6 +1,7 @@
 import * as anchor from '@coral-xyz/anchor';
 import { Program } from '@coral-xyz/anchor';
 import { AccountsAndSigners } from '../target/types/accounts_and_signers';
+import { assert } from 'chai';
 
 describe('accounts-and-signers', () => {
   // Configure the client to use the local cluster.
@@ -8,6 +9,9 @@ describe('accounts-and-signers', () => {
 
   const program = anchor.workspace
     .AccountsAndSigners as Program<AccountsAndSigners>;
+
+  let myKeyPair1 = anchor.web3.Keypair.generate();
+  let myKeyPair2 = anchor.web3.Keypair.generate();
 
   it('Is initialized!', async () => {
     // Add your test here.
@@ -25,5 +29,48 @@ describe('accounts-and-signers', () => {
 
     console.log('Your transaction signature', tx);
     console.log('The signer is : ', program.provider.publicKey.toBase58());
+  });
+
+  it('tests account signers', async () => {
+    const tx = await program.methods
+      .callFromMultipleSigners()
+      .accounts({
+        signer: program.provider.publicKey,
+        signer2: myKeyPair1.publicKey,
+        signer3: myKeyPair2.publicKey,
+      })
+      .signers([myKeyPair1, myKeyPair2])
+      .rpc();
+
+    console.log('Your transaction signature', tx);
+    console.log('The signer is : ', program.provider.publicKey.toBase58());
+    console.log('The signer2 is : ', myKeyPair1.publicKey.toBase58());
+    console.log('The signer3 is : ', myKeyPair2.publicKey.toBase58());
+  });
+
+  it('tests only owner function', async () => {
+    const tx = await program.methods
+      .onlyOwnerFunction()
+      .accounts({
+        signer: program.provider.publicKey,
+      })
+      .rpc();
+    console.log('Your transaction signature', tx);
+
+    try {
+      await program.methods
+        .onlyOwnerFunction()
+        .accounts({
+          signer: myKeyPair1.publicKey,
+        })
+        .signers([myKeyPair1])
+        .rpc();
+    } catch (_err) {
+      let err: anchor.AnchorError = _err;
+      const errMsg = 'Only the owner can call this function';
+      assert.strictEqual(err.error.errorMessage, errMsg);
+      console.log('Error number: ', err.error.errorCode.number);
+      console.log('Error code: ', err.error.errorCode.code);
+    }
   });
 });
